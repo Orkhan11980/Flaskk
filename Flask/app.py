6f25ca26e73dce77733096f6d8396d9fc84e8927
Orkhan11980 <115529584+Orkhan11980@users.noreply.github.com>
2023-10-28 14:55:01 +0400

    Commit

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,13 @@
 from flask import request
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, jsonify
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date
 from flask_migrate import Migrate
+from wtforms.widgets import TextArea
+from werkzeug.security import generate_password_hash, check_password_hash
 import psycopg2
 
 app = Flask(__name__)
@@ -17,12 +19,78 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    slug = db.Column(db.String(255))
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+    author = StringField("Author", validators=[DataRequired()])
+    slug = StringField("Slug", validators=[DataRequired()])
+    submit = SubmitField("Submit", validators=[DataRequired()])
+
+
+@app.route('/posts')
+def posts():
+    posts = Posts.query.order_by(Posts.date_posted)
+    return render_template("posts.html", posts=posts)
+
+@app.route('/add-post', methods=['GET', 'POST'])
+def add_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Posts(title=form.title.data, content=form.content.data,
+                     author=form.author.data, slug=form.slug.data)
+        form.title.data = ''
+        form.content.data = ''
+        form.author.data = ''
+        form.slug.data = ''
+
+        db.session.add(post)
+        db.session.commit()
+
+        flash("blog submitted")
+
+    return render_template("add_post.html", form=form)
+
+
+@app.route('/date')
+def get_current_date():
+    fav_pizza = {
+        "jon": "pape",
+        "mary": "cheese"
+    }
+    return jsonify(fav_pizza)
+
+
+#  return jsonify(date.today())
+
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     favori_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(256))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<Name %r>' % self.name
@@ -32,6 +100,9 @@ class UserForm(FlaskForm):
     name = StringField("NAME", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favori_color = StringField("favori color")
+    password_hash = PasswordField('Password',
+                                  validators=[DataRequired(), EqualTo('password_hash2', message='password must match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("submit")
 
 
@@ -92,6 +163,12 @@ class NameForm(FlaskForm):
     submit = SubmitField("submit")
 
 
+class PasswordForm(FlaskForm):
+    email = StringField("your email", validators=[DataRequired()])
+    password_hash = PasswordField("your password?", validators=[DataRequired()])
+    submit = SubmitField("submit")
+
+
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
     name = None
@@ -100,13 +177,16 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data, favori_color=form.favori_color.data)
+            hashed_pw = generate_password_hash(form.password_hash.data)
+            user = Users(name=form.name.data, email=form.email.data, favori_color=form.favori_color.data,
+                         password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
         form.favori_color.data = ''
+        form.password_hash.data = ''
         flash("User added")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html",
@@ -142,6 +222,29 @@ def page_not_found(e):
     return render_template("500.html"), 500
 
 
+@app.route('/test_pw', methods=['GET', 'POST'])
+def test_pw():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+    form = PasswordForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password_hash.data
+        form.email.data = ''
+        form.password_hash.data = ''
+        pw_to_check = Users.query.filter_by(email=email).first()
+
+        passed = check_password_hash(pw_to_check.password_hash, password)
+    return render_template("test_pw.html",
+                           email=email,
+                           password=password,
+                           pw_to_check=pw_to_check,
+                           passed=passed,
+                           form=form)
+
+
 @app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
@@ -150,6 +253,7 @@ def name():
         name = form.name.data
         form.name.data = ''
         flash("succesfuly submit")
+
     return render_template("name.html",
                            name=name,
                            form=form)
